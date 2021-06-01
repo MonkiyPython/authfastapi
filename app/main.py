8be166b506ac3fb7bdc3fd6038409f6e43805913
MonkiyPython <rajath@monkypython.com>
2021-06-01 16:31:15 +0530

    Need to do Dine Alteration

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.api.api_v1 import api
 from app.core.config import settings
 from app.db.database import engine, Base
-from app.models.customer import Customer
+from app.models.customer import Customer  # noqa  # Need do Fine Alteration
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
